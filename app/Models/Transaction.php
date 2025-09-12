15601d0965772249e98cb5763cd29f67d7082331
rofiareiv <kambing.rofi@gmail.com>
2025-09-12 09:54:42 +0700

    update transaction model

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -9,4 +9,24 @@ class Transaction extends Model
     protected $table = 'transactions';
 
     protected $guarded = ['id'];
+
+    public function sourceAccount()
+    {
+        return $this->belongsTo(Account::class, 'source_account', 'account_number');
+    }
+
+    public function destinationAccount()
+    {
+        return $this->hasMany(Account::class, 'destination_account', 'account_number');
+    }
+
+    public function transactionType()
+    {
+        return $this->belongsTo(TransactionType::class, 'transaction_type', 'id');
+    }
+
+    public function ledgerEntries()
+    {
+        return $this->hasMany(TransactionLedger::class, 'transaction_id', 'id');
+    }
 }
