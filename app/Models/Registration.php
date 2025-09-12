e75d8d2e2f32e5175be611dbe776d0e92928500d
rofiareiv <kambing.rofi@gmail.com>
2025-09-12 08:42:36 +0700

    update student and registration controller

--- app/Models/Registration.php
+++ app/Models/Registration.php
@@ -7,4 +7,20 @@ use Illuminate\Database\Eloquent\Model;
 class Registration extends Model
 {
     protected $guarded = ['id'];
+
+
+    public function parent()
+    {
+        return $this->belongsTo(ParentProfile::class);
+    }
+
+    public function program()
+    {
+        return $this->belongsTo(Program::class, 'program_id');
+    }
+
+    public function files()
+    {
+        return $this->hasMany(RegistrationFile::class);
+    }
 }
