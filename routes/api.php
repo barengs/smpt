da6ba27eb19c9e85e92459aae9bbdcd04422b3f3
rofiareiv <kambing.rofi@gmail.com>
2025-09-16 12:59:40 +0700

    update route profile

--- routes/api.php
+++ routes/api.php
@@ -51,7 +51,7 @@ Route::group([
     Route::post('login', [AuthController::class, 'login']);
     Route::post('logout', [AuthController::class, 'logout']);
     Route::post('refresh', [AuthController::class, 'refresh']);
-    Route::post('me', [AuthController::class, 'me']);
+    Route::post('profile', [AuthController::class, 'me']);
     Route::post('register', [AuthRegisterController::class, 'register']);
 
 });
