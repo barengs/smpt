f990fb531dfced1d900b745a8a3eef31e38014bd
rofiareiv <kambing.rofi@gmail.com>
2025-09-19 16:23:35 +0700

    perbaikan relasi tabel user ke orang tua

--- app/Models/ParentProfile.php
+++ app/Models/ParentProfile.php
@@ -10,4 +10,9 @@ class ParentProfile extends Model
     use SoftDeletes;
 
     protected $guarded = ['id'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
