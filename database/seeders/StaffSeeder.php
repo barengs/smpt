84aaa4482ceef3cbc7c12ebbd77e2faf949f4888
rofiareiv <kambing.rofi@gmail.com>
2026-01-24 21:30:11 +0700

    perbaikan fitur import orangtua dan menghapus user

--- database/seeders/StaffSeeder.php
+++ database/seeders/StaffSeeder.php
@@ -10,10 +10,6 @@ class StaffSeeder extends Seeder
 {
     protected $data = [
         ["code" => "AS002025", "first_name" => "Admin", "email" => "admin@gmail.com", "last_name" => "Admin", "user_id" => 1],
-        ["code" => "AS012025", "first_name" => "RAHMAN", "email" => "rahman@gmail.com", "last_name" => "FARUQ", "user_id" => 2],
-        ["code" => "AS022025", "first_name" => "RUMHUL", "email" => "rumhul@gmail.com", "last_name" => "AMIN", "user_id" => 3],
-        ["code" => "AS032025", "first_name" => "FATHUR", "email" => "fathur@gmail.com", "last_name" => "ROZI", "user_id" => 4],
-        ["code" => "AS042025", "first_name" => "GHUFRON", "email" => "ghufron@gmail.com", "last_name" => "DPU", "user_id" => 5],
     ];
     /**
      * Run the database seeds.
